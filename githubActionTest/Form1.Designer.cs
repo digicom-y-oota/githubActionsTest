c2f7eae304a27aef9551489be9f14452f4d766c4
y-oota <y-oota@tbeye.com>
2022-05-17 16:53:53 +0900

    first Commit

---
+++ githubActionTest/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace githubActionTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textClick = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textClick
+            // 
+            this.textClick.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textClick.AutoSize = true;
+            this.textClick.Font = new System.Drawing.Font("MS UI Gothic", 60F);
+            this.textClick.Location = new System.Drawing.Point(73, 178);
+            this.textClick.Name = "textClick";
+            this.textClick.Size = new System.Drawing.Size(622, 80);
+            this.textClick.TabIndex = 0;
+            this.textClick.Text = "GithubActionTest";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textClick);
+            this.Name = "Form1";
+            this.Text = "GithubActionTest";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label textClick;
+    }
+}
+
